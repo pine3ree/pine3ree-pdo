1b86442912274eba54727a77a13d3cd05e97b525
pine3ree <pine3ree@gmail.com>
2023-10-04 02:41:58 +0200

    Add profiling pdo-statement tests
    Signed-off-by: pine3ree <pine3ree@gmail.com>

---
+++ test/Profiling/PDOStatementTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @package   pine3ree-pdo
+ * @see       https://github.com/pine3ree/pine3ree-pdo for the canonical source repository
+ * @copyright https://github.com/pine3ree/pine3ree-pdo/blob/master/COPYRIGHT.md
+ * @author    pine3ree https://github.com/pine3ree
+ * @license   https://github.com/pine3ree/pine3ree-pdo/blob/master/LICENSE.md New BSD License
+ */
+
+namespace pine3ree\PDOTest\Profiling;
+
+use pine3ree\PDOTest\Profiling\AbstractPDOTest;
+use pine3ree\PDO as LazyPDO;
+use pine3ree\PDO\Profiling\PDO;
+use pine3ree\PDO\Profiling\PDOStatement;
+use ReflectionClass;
+
+final class PDOStatementTest extends AbstractPDOTest
+{
+    protected static function expectedStatementClass(): string
+    {
+        return PDOStatement::class;
+    }
+
+    protected function createPDO(): PDO
+    {
+        return new PDO(new \PDO($this->dsn, '', ''));
+    }
+
+    protected function createLazyPDO(): PDO
+    {
+        return new PDO(new LazyPDO($this->dsn, '', ''));
+    }
+
+    // phpcs:disable
+
+    public function test_method_bindValue()
+    {
+        $pdo = $this->createPDO();
+
+        $stmt = $pdo->prepare("SELECT * FROM `user` WHERE `id` = :id");
+
+        $rc = new ReflectionClass($stmt);
+        $rp = $rc->getProperty('params');
+        $rp->setAccessible(true);
+
+        $id = 42;
+
+        $stmt->bindValue('id', $id);
+
+        $params = $rp->getValue($stmt);
+        self::assertSame($id, $params['id'] ?? null);
+
+        $stmt->execute();
+    }
+
+    public function test_method_bindParam()
+    {
+        $pdo = $this->createPDO();
+
+        $stmt = $pdo->prepare("SELECT * FROM `user` WHERE `id` = :id");
+
+        $rc = new ReflectionClass($stmt);
+        $rp = $rc->getProperty('params');
+        $rp->setAccessible(true);
+
+        $stmt->bindParam('id', $id);
+
+        $params = $rp->getValue($stmt);
+        self::assertSame($id, $params['id'] ?? null);
+
+        $stmt->execute();
+    }
+
+
+    // phpcs:enable
+}
