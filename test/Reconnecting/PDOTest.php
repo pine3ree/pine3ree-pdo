af3d664725988ef750c1b5d8e84f0604359cb9da
pine3ree <pine3ree@gmail.com>
2023-10-04 04:41:05 +0200

    Add test cases for reconnectiong pdo setTTL()
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- test/Reconnecting/PDOTest.php
+++ test/Reconnecting/PDOTest.php
@@ -13,6 +13,7 @@ namespace pine3ree\PDOTest\Reconnecting;
 use InvalidArgumentException;
 use pine3ree\PDO\Reconnecting\PDO;
 use pine3ree\PDOTest\Profiling\AbstractPDOTest;
+use LogicException;
 use PDOException;
 use ReflectionClass;
 
@@ -28,6 +29,15 @@ final class PDOTest extends AbstractPDOTest
     protected function createReconnectingPDO(int $ttl): PDO
     {
         return new PDO($this->dsn, '', '', [], $ttl);
+    }
+
+    protected function getDecoratedPDO(PDO $pdo): \PDO
+    {
+        $rc = new ReflectionClass($pdo);
+        $rm = $rc->getMethod('pdo');
+        $rm->setAccessible(true);
+
+        return $rm->invoke($pdo);
     }
 
     public function testThatCreatingInstanceWithNonPositiveTtlRisesInvalidArgumentException()
@@ -103,12 +113,8 @@ final class PDOTest extends AbstractPDOTest
         $fetchMode = \PDO::FETCH_ASSOC;
         $pdo = new PDO('mysql://db=non-existent', '', '', [], $ttl);
 
-        $rc = new ReflectionClass(PDO::class);
-        $rm = $rc->getMethod('pdo');
-        $rm->setAccessible(true);
-
         $this->expectException(PDOException::class);
-        $rm->invoke($pdo);
+        $phpPdo = $this->getDecoratedPDO($pdo);
     }
 
     public function testThatGetAttributeWithCustomStringKeyRetunsValidTtlValue()
@@ -129,12 +135,9 @@ final class PDOTest extends AbstractPDOTest
 
         self::assertSame($fetchMode, $pdo->getAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE));
 
-        $rc = new ReflectionClass(PDO::class);
-        $rm = $rc->getMethod('pdo');
-        $rm->setAccessible(true);
+        $phpPdo = $this->getDecoratedPDO($pdo);
 
-        $wrappedPdo = $rm->invoke($pdo);
-        self::assertSame($fetchMode, $wrappedPdo->getAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE));
+        self::assertSame($fetchMode, $phpPdo->getAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE));
     }
 
     public function testThatGetAttributeWithInvalidKeyTypeRaisesException()
@@ -161,7 +164,7 @@ final class PDOTest extends AbstractPDOTest
         $pdo->setAttribute('ttl', 'ABC');
     }
 
-    public function testThatSetTtlAttributeWithValue()
+    public function testThatSetTtlAttributeWithValidIntValueSucceds()
     {
         $pdo = $this->createReconnectingPDO(1);
         $pdo->setAttribute('ttl', 42);
@@ -175,5 +178,55 @@ final class PDOTest extends AbstractPDOTest
         $pdo = $this->createReconnectingPDO($ttl);
 
         self::assertSame($ttl, $pdo->getTTL());
+    }
+
+    public function testSetTTL()
+    {
+        $pdo = $this->createReconnectingPDO(42);
+        $pdo->setTTL(43);
+
+        self::assertSame(43, $pdo->getTTL());
+    }
+
+    public function testSettingTtlMultipleTimesBeforConnectingWorks()
+    {
+        $pdo = $this->createReconnectingPDO(42);
+
+        $pdo->setTTL(43);
+        self::assertSame(43, $pdo->getTTL());
+        self::assertSame(43, $pdo->getAttribute($pdo::ATTR_CONNECTION_TTL));
+
+        $pdo->setTTL(44);
+        self::assertSame(44, $pdo->getTTL());
+        self::assertSame(44, $pdo->getAttribute($pdo::ATTR_CONNECTION_TTL));
+
+        $pdo->setTTL(45);
+        self::assertSame(45, $pdo->getTTL());
+        self::assertSame(45, $pdo->getAttribute($pdo::ATTR_CONNECTION_TTL));
+
+        $pdo->setAttribute($pdo::ATTR_CONNECTION_TTL, 41);
+        self::assertSame(41, $pdo->getAttribute($pdo::ATTR_CONNECTION_TTL));
+        self::assertSame(41, $pdo->getTTL());
+
+        $pdo->setAttribute($pdo::ATTR_CONNECTION_TTL, 40);
+        self::assertSame(40, $pdo->getAttribute($pdo::ATTR_CONNECTION_TTL));
+        self::assertSame(40, $pdo->getTTL());
+
+        $pdo->setAttribute($pdo::ATTR_CONNECTION_TTL, 39);
+        self::assertSame(39, $pdo->getAttribute($pdo::ATTR_CONNECTION_TTL));
+        self::assertSame(39, $pdo->getTTL());
+    }
+
+    public function testSettingTtlAfterFirstConnectionRaisesException()
+    {
+        $pdo = $this->createReconnectingPDO(42);
+
+        $this->getDecoratedPDO($pdo);
+
+        $this->expectException(LogicException::class);
+        $pdo->setTTL(43);
+
+        $this->expectException(LogicException::class);
+        $pdo->setAttribute('ttl', 43);
     }
 }
