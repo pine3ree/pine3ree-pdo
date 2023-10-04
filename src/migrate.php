c7c882b7f878e81785409883fee715644a6a8e21
pine3ree <pine3ree@gmail.com>
2023-10-04 18:17:13 +0200

    Add composer-autoloaded migration file with class aliases
    Signed-off-by: pine3ree <pine3ree@gmail.com>

---
+++ src/migrate.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Temporary class aliases, will be removed in next versions
+ */
+
+class_alias(pine3ree\PDO::class, P3\PDO::class);
+class_alias(pine3ree\PDO\Profiling\PDO::class, P3\PDO\Profiling\PDO::class);
+class_alias(pine3ree\PDO\Profiling\PDOStatement::class, P3\PDO\Profiling\PDOStatement::class);
+class_alias(pine3ree\PDO\Reconnecting\PDO::class, P3\PDO\Reconnecting\PDO::class);
